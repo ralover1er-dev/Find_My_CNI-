33aab94f9a9a5aafc43387b8d46acd377f5514e9
ralover1er-dev <r.a.lover1er@gmail.com>
2026-08-11 05:54:03 +0100

    Rename package and update MainActivity class

--- Find My CNI/find_my_cni_flutter/android/app/src/main/kotlin/cm/findmycni/app/MainActivity.kt
+++ Find My CNI/find_my_cni_flutter/android/app/src/main/kotlin/cm/findmycni/app/MainActivity.kt
@@ -1,5 +1,6 @@
-package cm.findmycni.app
+package com.example.find_my_cni
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
